b0017dac34502d3128526a14fd485ba868b42e5b
Sandeep <patelsandeep88@gmail.com>
2021-11-10 23:44:49 +0530

    1st Commit in Linux os Version

--- Main.py
+++ Main.py
@@ -13,15 +13,14 @@ while True:
     print('''
 Enter 1 for date Print
 Enter 2 for calender Print
-Enter 3 for NotePad Open
-Enter 4 for Chrome Open
+Enter 3 for Text Editer Open
+Enter 4 for Firefox Open
 Enter 5 for Minikube launch
 Enter 6 for launch Google
 Enter 7 for launch Python
 Enter 8 for Create EC2 instances in us-east-1 resion
 Enter 9 for launch Apache server for hosting the webpage
-Enter 10 to show your web page
-Enter 11 to launch docker
+Enter 10 to launch docker
 
 ''')
     if c == "y" :
@@ -68,7 +67,7 @@ Enter 11 to launch docker
             print("*"*80)
         elif ch == 10:
             import webbrowser
-            webbrowser.open('file:///C:/Users/Sandeep/Desktop/Arth-2.0-Python-Menu-project/Arth-2.0-Python-Menu-project/index.html')
+            #webbrowser.open('file:///C:/Users/Sandeep/Desktop/Arth-2.0-Python-Menu-project/Arth-2.0-Python-Menu-project/index.html')
             print("*"*80)
             print("thank you for using")
             print("*"*80)    
